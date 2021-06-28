e594d4bff74cb27cf8eb55b08c0a2f83ffd21d8f
Mawanda Peter Pascal <mawandapascal@gmail.com>
2021-06-28 21:36:09 +0300

    simple changes

--- lib/screens/details_screen.dart
+++ lib/screens/details_screen.dart
@@ -16,162 +16,235 @@ class CharDetails extends StatelessWidget {
     double height = MediaQuery.of(context).size.height;
     var data = Get.arguments;
 
-    return Scaffold(
-        body: SingleChildScrollView(
-          child: Container(
-            width: width,
-            child: Stack(
-              children: [
-                Container(
-                    height: height * 0.55,
-                    decoration: BoxDecoration(
-                      image: DecorationImage(
-                        image: NetworkImage(data[3]),
-                        fit: BoxFit.cover,
+    return SafeArea(
+      child: Scaffold(
+          body: SingleChildScrollView(
+            child: Container(
+              width: width,
+              child: Stack(
+                children: [
+                  Container(
+                      height: height * 0.55,
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: NetworkImage(data[3]),
+                          fit: BoxFit.cover,
+                        ),
                       ),
                     ),
-                  ),
 
-                Container(
-                  width: width,
-                  margin: EdgeInsets.only(top: height*0.5),
-                  padding: EdgeInsets.all(30),
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.only(
-                      topRight: Radius.circular(30),
-                      topLeft: Radius.circular(30),
-                    ),),
+                  Container(
+                    width: width,
+                    margin: EdgeInsets.only(top: height*0.5),
+                    padding: EdgeInsets.all(30),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.only(
+                        topRight: Radius.circular(30),
+                        topLeft: Radius.circular(30),
+                      ),),
 
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Wrap(
-                        children: [
-                          Padding(
-                            padding: const EdgeInsets.only(top: 10),
-                            child: RichText(text: TextSpan(
-                                children: [
-                                  TextSpan(
-                                    text: 'C',
-                                    style: TextStyle(
-                                      color: Colors.green,
-                                      fontWeight: FontWeight.bold,
-                                      fontSize: 20.sp,),),
-                                  TextSpan(
-                                    text: 'haracter : ',
-                                    style: TextStyle(
-                                      color: Colors.black,
-                                      fontSize: 15.sp,),),
-                                  TextSpan(
-                                    text: '${data[0]}',
-                                    style: TextStyle(
-                                      color: Colors.black,
-                                      fontSize: 23.sp,
-                                    fontWeight: FontWeight.bold),),
-                                ]
-                            ),),
-                          ),
-                        ],
-                      ),
-                      Wrap(
-                        children: [
-                          Padding(
-                            padding: const EdgeInsets.only(top: 10),
-                            child: RichText(text: TextSpan(
-                                children: [
-                                  TextSpan(
-                                    text: 'Ni',
-                                    style: TextStyle(
-                                      color: Colors.green,
-                                      fontWeight: FontWeight.bold,
-                                      fontSize: 20.sp,),),
-                                  TextSpan(
-                                    text: 'ckname : ',
-                                    style: TextStyle(
-                                      color: Colors.black,
-                                      fontSize: 15.sp,),),
-                                  TextSpan(
-                                    text: '${data[5]}',
-                                    style: TextStyle(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Wrap(
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.only(top: 10),
+                              child: RichText(text: TextSpan(
+                                  children: [
+                                    TextSpan(
+                                      text: 'C',
+                                      style: TextStyle(
+                                        color: Colors.green,
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 20.sp,),),
+                                    TextSpan(
+                                      text: 'haracter : ',
+                                      style: TextStyle(
                                         color: Colors.black,
-                                        fontSize: 20.sp,
-                                        fontWeight: FontWeight.w400),),
-                                ]
-                            ),),
-                          ),
-                        ],
-                      ),
-                      Wrap(
-                        children: [
-                          Padding(
-                            padding: const EdgeInsets.only(top: 10),
-                            child: RichText(text: TextSpan(
-                                children: [
-                                  TextSpan(
-                                    text: 'O',
-                                    style: TextStyle(
-                                      color: Colors.green,
-                                      fontWeight: FontWeight.bold,
-                                      fontSize: 20.sp,),),
-                                  TextSpan(
-                                    text: 'ccupation : ',
-                                    style: TextStyle(
-                                      color: Colors.black,
-                                      fontSize: 15.sp,),),
-                                  TextSpan(
-                                    text: '${data[2]}',
-                                    style: TextStyle(
+                                        fontSize: 15.sp,),),
+                                    TextSpan(
+                                      text: '${data[0]}',
+                                      style: TextStyle(
                                         color: Colors.black,
                                         fontSize: 23.sp,
-                                        fontWeight: FontWeight.bold),),
-                                ]
-                            ),),
+                                      fontWeight: FontWeight.bold),),
+                                  ]
+                              ),),
+                            ),
+                          ],
+                        ),
+                        Wrap(
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.only(top: 10),
+                              child: RichText(text: TextSpan(
+                                  children: [
+                                    TextSpan(
+                                      text: 'Ni',
+                                      style: TextStyle(
+                                        color: Colors.green,
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 20.sp,),),
+                                    TextSpan(
+                                      text: 'ckname : ',
+                                      style: TextStyle(
+                                        color: Colors.black,
+                                        fontSize: 15.sp,),),
+                                    TextSpan(
+                                      text: '${data[5]}',
+                                      style: TextStyle(
+                                          color: Colors.black,
+                                          fontSize: 20.sp,
+                                          fontWeight: FontWeight.w400),),
+                                  ]
+                              ),),
+                            ),
+                          ],
+                        ),
+                        Wrap(
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.only(top: 10),
+                              child: RichText(text: TextSpan(
+                                  children: [
+                                    TextSpan(
+                                      text: 'O',
+                                      style: TextStyle(
+                                        color: Colors.green,
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 20.sp,),),
+                                    TextSpan(
+                                      text: 'ccupation(s) : ',
+                                      style: TextStyle(
+                                        color: Colors.black,
+                                        fontSize: 15.sp,),),
+                                    TextSpan(
+                                      text: '${data[2]}',
+                                      style: TextStyle(
+                                          color: Colors.black,
+                                          fontSize: 23.sp,
+                                          fontWeight: FontWeight.w300),),
+                                  ]
+                              ),),
+                            ),
+                          ],
+                        ),
+                        Wrap(
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.only(top: 10),
+                              child: RichText(text: TextSpan(
+                                  children: [
+                                    TextSpan(
+                                      text: 'Se',
+                                      style: TextStyle(
+                                        color: Colors.green,
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 20.sp,),),
+                                    TextSpan(
+                                      text: 'ason(s) : ',
+                                      style: TextStyle(
+                                        color: Colors.black,
+                                        fontSize: 13.sp,),),
+                                    TextSpan(
+                                      text: '${data[6]}',
+                                      style: TextStyle(
+                                          color: Colors.black,
+                                          fontSize: 23.sp,
+                                          fontWeight: FontWeight.w300),),
+                                  ]
+                              ),),
+                            ),
+                          ],
+                        ),
+                        ListTile(
+                          leading: Wrap(
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.only(top: 10),
+                                child: RichText(text: TextSpan(
+                                    children: [
+                                      TextSpan(
+                                        text: 'S',
+                                        style: TextStyle(
+                                          color: Colors.green,
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 20.sp,),),
+                                      TextSpan(
+                                        text: 'tatus : ',
+                                        style: TextStyle(
+                                          color: Colors.black,
+                                          fontSize: 13.sp,),),
+
+                                    ]
+                                ),),
+                              ),
+                            ],
                           ),
-                        ],
-                      )
-                    ],
+                          title: Container(
+                            height: 14.h,
+                            width: 55.w,
+                            decoration: BoxDecoration(
+                                color: (data[4] =="Alive")
+                                    ? Colors.green
+                                    : Colors.red,
+                              borderRadius: BorderRadius.all(Radius.circular(35))
+                            ),
+                            child: Align(
+                              alignment: Alignment.center,
+                                child: Text(data[4],
+                                  style: TextStyle(
+                                    fontSize: 25.sp,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                  textAlign: TextAlign.center,)),
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
-                ),
-                Positioned(
-                  right: 30,
-                  top: height*0.43,
-                  child: Container(
-                    height: 85,
-                    width: 180,
-                    decoration: BoxDecoration(
-                        color: Colors.lightGreen,
-                      borderRadius: BorderRadius.all(Radius.circular(30)),
-                    ),
-                    child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Text(data[7], style: TextStyle(
-                        fontSize: 30,
-                        fontWeight: FontWeight.w400
-                      ),),
-                    ),
-                  ),),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: GestureDetector(
-                    onTap: (){
-                      Get.back();
-                    },
-                    child: Align(
-                      alignment: Alignment.topLeft,
-                      child: Icon(
-                        Icons.arrow_back_outlined,
-                        color: Colors.black,
-                        size: 50,
+                  Positioned(
+                    right: 30,
+                    top: height*0.43,
+                    child: Container(
+                      height: 16.h,
+                      width: 55.w,
+                      decoration: BoxDecoration(
+                          color: Colors.lightGreen,
+                        borderRadius: BorderRadius.all(Radius.circular(30)),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(data[7], style: TextStyle(
+                          fontSize: 25.sp,
+                          fontWeight: FontWeight.w400
+                        ),),
+                      ),
+                    ),),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: GestureDetector(
+                      onTap: (){
+                        Get.back();
+                      },
+                      child: Align(
+                        alignment: Alignment.topLeft,
+                        child: Icon(
+                          Icons.arrow_back_outlined,
+                          color: Colors.white,
+                          size: 50,
+                        ),
                       ),
                     ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
-        ),
-        );
+          ),
+    );
   }
 
 }
--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -17,65 +17,67 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.black,
-      appBar: AppBar(
-        title: RichText(text: TextSpan(
-          children: [
-            TextSpan(
-              text: 'B',
-              style: TextStyle(
-                color: Colors.green,
-                fontWeight: FontWeight.bold,
-                fontSize: 20.sp,),),
-            TextSpan(
-              text: 'eaking ',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 15.sp,),),
-            TextSpan(
-              text: 'Ba',
-              style: TextStyle(
-                color: Colors.green,
-                fontWeight: FontWeight.bold,
-                fontSize: 20.sp,),),
-            TextSpan(
-              text: 'd',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 15.sp,),),
-          ]
-        )),
-        elevation: 0,
-        centerTitle: true,
-      ),
-      body: SizedBox.expand(
-        child: PageView(
-          children: [
-            CharacterGridView(),
-            EpisodesScreen(),
-            QuotesScreen(),
-          ],
-          controller: homePageController.pageController,
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.black,
+        appBar: AppBar(
+          title: RichText(text: TextSpan(
+            children: [
+              TextSpan(
+                text: 'B',
+                style: TextStyle(
+                  color: Colors.green,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20.sp,),),
+              TextSpan(
+                text: 'eaking ',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 15.sp,),),
+              TextSpan(
+                text: 'Ba',
+                style: TextStyle(
+                  color: Colors.green,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20.sp,),),
+              TextSpan(
+                text: 'd',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 15.sp,),),
+            ]
+          )),
+          elevation: 0,
+          centerTitle: true,
         ),
-      ),
-      bottomNavigationBar: Obx(() =>
-          FancyBottomNavigation(
-          tabs: [
-            TabData(iconData: Icons.home, title: 'Home'),
-            TabData(iconData: Icons.tv_sharp, title: 'Episodes'),
-            TabData(iconData: Icons.format_quote_outlined, title: 'Quotes'),
-          ],
-          onTabChangedListener: (position) {
-            homePageController.currentIndex.value = position;
-            homePageController.pageController.jumpToPage(position);
-          },
-          initialSelection: homePageController.currentIndex.value,
-          key: homePageController.bottomNavKey,
-          inactiveIconColor: Colors.black,
-          activeIconColor: Colors.green,
-          barBackgroundColor: Colors.amber,
-          textColor: Colors.black,
+        body: SizedBox.expand(
+          child: PageView(
+            children: [
+              CharacterGridView(),
+              EpisodesScreen(),
+              QuotesScreen(),
+            ],
+            controller: homePageController.pageController,
+          ),
+        ),
+        bottomNavigationBar: Obx(() =>
+            FancyBottomNavigation(
+            tabs: [
+              TabData(iconData: Icons.home, title: 'Home'),
+              TabData(iconData: Icons.tv_sharp, title: 'Episodes'),
+              TabData(iconData: Icons.format_quote_outlined, title: 'Quotes'),
+            ],
+            onTabChangedListener: (position) {
+              homePageController.currentIndex.value = position;
+              homePageController.pageController.jumpToPage(position);
+            },
+            initialSelection: homePageController.currentIndex.value,
+            key: homePageController.bottomNavKey,
+            inactiveIconColor: Colors.black,
+            activeIconColor: Colors.green,
+            barBackgroundColor: Colors.amber,
+            textColor: Colors.black,
+          ),
         ),
       ),
     );
